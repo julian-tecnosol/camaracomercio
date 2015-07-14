c0e3f514d6df3278d5b8b54345eb57cad2bdad58
Julian Albero <desarrollo@tecnosolysoft.com>
2015-07-13 21:05:43 -0500

    Commit de prueba

--- js/validador.js
+++ js/validador.js
@@ -2,17 +2,34 @@
  * Created by Julian Albero on 13/07/2015.
  */
 function validador(){
-    var allInput = $("#formContainer").find("input");
+    $("#formContainer").submit(function(event){
+        event.preventDefault();
+        var formulario = $("#formContainer");
+        var allInputText = formulario.find("input[type='text']");
+        var allInputRadio = formulario.find("input[type='radio'],input[type='checkbox']");
 
-    var control = allInput.length;
+        var controlText = allInputText.length;
+        var controlRadio = allInputText.length;
 
-    for(var i = 0; i < control; i++){
-        console.log(allInput[i]);
-        var thisInput = $(allInput[i]);
-        if(thisInput.val().length >= 1){
-            thisInput.hide();
+        for(var i = 0; i < controlText; i++){
+            console.log(allInputText[i]);
+            var thisInputText = $(allInputText[i]);
+            if(thisInputText.val().length >= 1){
+                thisInputText.hide();
+            }else{
+                thisInputText.addClass('error');
+            }
         }
-    }
+
+        for(var j = 0; j < controlRadio; j++){
+            var thisInputRadio = $(allInputRadio[j]);
+            if(thisInputRadio.is(':checked')){
+                thisInputRadio.hide();
+            }else{
+                thisInputRadio.addClass('error');
+            }
+        }
+    });
 }
 
 $(document).ready(validador);
