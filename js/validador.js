2e53764c4f2359578cf73481e320da21fde7e37a
fronDevelop <desarrollo2@tecnosolysoft.com>
2015-07-13 21:13:56 -0500

    Cambio en el js

--- js/validador.js
+++ js/validador.js
@@ -2,7 +2,34 @@
  * Created by Julian Albero on 13/07/2015.
  */
 function validador(){
-    
+    $("#formContainer").submit(function(event){
+        event.preventDefault();
+        var formulario = $("#formContainer");
+        var allInputText = formulario.find("input[type='text']");
+        var allInputRadio = formulario.find("input[type='radio'],input[type='checkbox']");
+
+        var controlText = allInputText.length;
+        var controlRadio = allInputText.length;
+
+        for(var i = 0; i < controlText; i++){
+            console.log(allInputText[i]);
+            var thisInputText = $(allInputText[i]);
+            if(thisInputText.val().length >= 1){
+                thisInputText.hide();
+            }else{
+                thisInputText.addClass('error');
+            }
+        }
+
+        for(var j = 0; j < controlRadio; j++){
+            var thisInputRadio = $(allInputRadio[j]);
+            if(thisInputRadio.is(':checked')){
+                thisInputRadio.hide();
+            }else{
+                thisInputRadio.addClass('error');
+            }
+        }
+    });
 }
 
 $(document).ready(validador);
