080e8081ebbf430cbaabf5a06439ae6913abb8c4
Julian Albero <desarrollo@tecnosolysoft.com>
2015-07-13 20:27:40 -0500

    Codigo faltante en el index

--- js/validador.js
+++ js/validador.js
@@ -1,3 +1,18 @@
 /**
  * Created by Julian Albero on 13/07/2015.
  */
+function validador(){
+    var allInput = $("#formContainer").find("input");
+
+    var control = allInput.length;
+
+    for(var i = 0; i < control; i++){
+        console.log(allInput[i]);
+        var thisInput = $(allInput[i]);
+        if(thisInput.val().length >= 1){
+            thisInput.hide();
+        }
+    }
+}
+
+$(document).ready(validador);
